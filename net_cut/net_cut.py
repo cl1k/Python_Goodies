189522ac2a8bf15ced6145c613e51d54ecc19d95
root <alexmlake@gmail.com>
2019-05-08 17:31:53 -0400

    iptables commands

--- net_cut/net_cut.py
+++ net_cut/net_cut.py
@@ -2,11 +2,14 @@
 import netfilterqueue
 # sudo apt install libnetfilter-queue-dev
 # pip install netfilterqueue
+# iptables -I FORWARD -j NFQUEUE --queue -num 0
+# iptables --flush
 
 
 def process_packet(packet):
     print(packet)
-    packet.accept()
+    packet.drop()
+
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
